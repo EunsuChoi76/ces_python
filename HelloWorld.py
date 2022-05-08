90e458c1e2e3fa42e7809eebdeaac20c53237288
EunsuChoi76 <dmstnchl@gmail.com>
2022-05-08 23:31:52 +0900

    Create HelloWorld.py

---
+++ HelloWorld.py
@@ -0,0 +1,16 @@
+
+def main():
+
+
+    data = dict()
+    data['사과'] = 'Apple'
+    data['바나나'] = 'Banana'
+    data['코코넛'] = 'Coconut'
+
+    print(data)
+
+
+
+
+if __name__ == "__main__":
+    main()
